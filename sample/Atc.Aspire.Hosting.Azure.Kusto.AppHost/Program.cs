12a8490025f3ee317c35f3ca7e0ffbd43d3a0f36
Per Kops <pko@delegate.dk>
2025-04-11 09:44:22 +0200

    feat: add WithUrls to api resource in aspire apphost to re-write swagger urls in aspire dashboard

--- sample/Atc.Aspire.Hosting.Azure.Kusto.AppHost/Program.cs
+++ sample/Atc.Aspire.Hosting.Azure.Kusto.AppHost/Program.cs
@@ -10,6 +10,22 @@ builder.AddProject<Atc_Aspire_Hosting_Azure_Kusto_ApiService>("apiservice")
     .WithReference(kusto)
     .WaitFor(kusto)
     .WithEnvironment("KustoDatabaseName", KustoContainerResource.DefaultDbName)
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithUrls(context =>
+    {
+        foreach (var tuple in context.Urls
+                     .Select(url => (Url: url, Uri: new Uri(url.Url)))
+                     .OrderByDescending(_ => _.Uri.Scheme is "https")
+                     .Select((pair, index) => (pair.Url, pair.Uri.Scheme, Index: index)))
+        {
+            var (url, scheme, index) = tuple;
+
+            // Order HTTPS first.
+            var order = context.Urls.Count - 1 - index;
+
+            url.DisplayText = $"Swagger ({scheme.ToUpper(CultureInfo.InvariantCulture)})";
+            url.DisplayOrder = order;
+        }
+    });
 
 await builder.Build().RunAsync();
